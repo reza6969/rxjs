39711a4790a37d6440a426f5b1f24d634d54830c
r.soltani <infooffice1399@gmail.com>
2023-09-30 01:16:31 +0330

    Create an observable
    :-1:
    09.Create an observable
    :+1:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  observable$;
+
+  ngOnInit() {
+    this.observable$ = Observable.create((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+    this.observable$.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('this is the end')
+    );
+  }
+
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
 }
